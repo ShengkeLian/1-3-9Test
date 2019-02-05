6586337c644b08789894fd28b647ca473026a58b
ShengkeLian <47255741+ShengkeLian@users.noreply.github.com>
2019-02-05 13:25:43 -0500

    Add files via upload

--- 1-3-9NameGame.py
+++ 1-3-9NameGame.py
@@ -7,8 +7,8 @@ the classroom alonf with your code'''
 def who_is_this():
     name=raw_input('Please enter the name of someone in this class:')
     
-    if name=='Carlos':
-        print(name,' favorite phrase is : "Now the fun part is..."')
+    if name=='Shengke':
+        print(name,' favorite phrase is : "I never go back on my word, that is my nindo my ninja way"')
     
     return
     
